20e924ff8425b8a71fabfb3be6e4e93230fe1db3
igo <igoranatol870@gmail.com>
2022-05-15 19:06:22 +0300

    sdfsdf

---
+++ js/sal_or_buac1.js
@@ -0,0 +1,124 @@
+function sol_or_bu () {
+    try {
+        if(confirm("Можете покупать или отправл")) {
+            if(confirm("Тоько надо для начала зарегестрироваться, или вой")) {
+                if(confirm("Войт")) {
+                    if(confirm("Для этого вам понадобиться ввести своё им")) {
+                        var use_nm1 = {
+                            us_nm1: {
+                                us_na1:"igo",
+                                us_sr1:"romane",
+                                us_ag1:"19"
+                            }
+                        };
+                        if(use_nm1.hasOwnProperty("us_nm1.us_na1") == true && use_nm1.us_nm1.us_na1 == "igo") {
+                            if(confirm("Введите своё им")) {
+                                var us_chn1;
+                                var us_csr1;
+                                var us_cag1;
+                                if(confirm("Введите своё им")) {
+                                    us_chn1 = prompt("Введите своё им");
+                                    if(us_chn1 != use_nm1.us_nm1.us_na1 && us_chn1 != use_nm1.us_nm1.us_sr1) throw new Error("У вас ошибка, поле не иожет быть пуст");
+                                    else if(us_chn1 == use_nm1.us_nm1.us_na1 && us_chn1 != use_nm1.us_nm1.us_sr1) {
+                                        us_csr1 = prompt("Введите свою фамил");
+                                        if(us_csr1 != use_nm1.us_nm1.us_sr1 && us_csr1 != use_nm1.us_nm1.us_ag1) throw new Error("У вас ошибка, поле не иожет быть пуст");
+                                        else if(us_csr1 == use_nm1.us_nm1.us_sr1 && us_chn1 != use_nm1.us_nm1.us_ag1) {
+                                            us_cag1 = prompt("Введите свой возра");
+                                            if(us_cag1 != use_nm1.us_nm1.us_ag1 && us_cag1 != use_nm1.us_nm1.us_sr1) throw new Error("У вас ошибка, поле не иожет быть пуст");
+                                            else if(us_cag1 == use_nm1.us_nm1.us_ag1 && us_cag1 != use_nm1.us_nm1.us_sr1) {
+                                                if(confirm("Можете посмотреть свой бала")) {
+                                                    if(confirm("Внимание, функция посмотреть баланс, является платн")) {
+                                                        var us_lo1 = {
+                                                            us_l1:"13",
+                                                            us_l2:"19",
+                                                            us_l3:"11",
+                                                            us_l4:"190",
+                                                            us_l5:"135"
+                                                        };
+                                                        if(confirm("Вам плата будет назначаться в объек")) {
+
+                                                        } else {
+                                                            if(confirm("Вам плата будет назначаться в масси")) {
+                                                                var us_loarr1 = [us_lo1.us_l1, us_lo1.us_l2, us_lo1.us_l3, us_lo1.us_l4, us_lo1.us_l5];
+                                                                if(confirm("Теперь вам плата будет назначаться в маccи")) {
+                                                                    if(confirm("Посмотерть свой балан")) {
+                                                                        Object.defineProperty(use_nm1, "us_act1", {value:"1399", writable:true, enumerable:true, configurable:true});
+                                                                        if(use_nm1.hasOwnProperty("us_act1") == true && use_nm1.us_act1 == "1399") {
+                                                                            if(confirm("Ваш бала" + " " + use_nm1.us_act1 + "$")) {
+                                                                                if(confirm("Можете посмотреть список акци")) {
+                                                                                    var us_li1s = {
+                                                                                        us_lih1:"130",
+                                                                                        us_lih2:"115",
+                                                                                        us_lih3:"137",
+                                                                                        us_lih4:"199",
+                                                                                        us_lih5:"115"
+                                                                                    };
+                                                                                    if(us_li1s.hasOwnProperty("us_lih1") == true && us_li1s.hasOwnProperty("us_lih2") == true) {
+                                                                                        if(confirm("Вам список акций, отсортиро")) {
+                                                                                            if(confirm("Ну за это возмётся ещ1 дополнительная пла")) {
+                                                                                                us_li1sco = Object.create(us_li1s);
+                                                                                                if(us_li1sco.hasOwnProperty("us_lih1") == true && us_li1sco.hasOwnProperty("us_lih2") == true) {
+                                                                                                    us_li2so = [us_li1sco.us_lih1, us_li1sco.us_lih2, us_li1sco.us_lih3, us_li1sco.us_lih4, us_li1sco.us_lih5];
+                                                                                                    for(i in us_li2so) 
+                                                                                                        if(us_li2so[i] == 0 || us_li2so[i] == null) continue;
+                                                                                                        if(confirm(us_li2so + " " + us_li2so[i])) {
+                                                                                                            if(confirm("Можете выбра")) {
+                                                                                                                if(confirm("Напишите, что выбр")) {
+                                                                                                                    var sal_pr;
+                                                                                                                    sal_pr = prompt("Введите название акци");
+                                                                                                                    us_loarr1.push(190);
+                                                                                                                   if(sal_pr == us_li1s.us_lih1 || sal_pr == us_li1s.us_lih2) {
+                                                                                                                       if(sal_pr == us_li1s.us_lih1 && sal_pr == "130") {
+                                                                                                                           if(confirm("Покупаем эту акци")) {
+                                                                                                                               if(confirm("С вас сниется пла")) {
+                                                                                                                                us_loarr1.push(19);
+                                                                                                                                if(confirm("Сколько акций покупа")) {
+                                                                                                                                    if(confirm("Введите количетсво, которое вы поку")) {
+                                                                                                                                        us_loarr1.push(0);
+                                                                                                                                        us_loarr1[7] = eval("us_li1s.us_lih1 * 10");
+                                                                                                                                        if(confirm("Вы купи")) {
+                                                                                                                                            if(confirm("Посмотреть плат")) {
+                                                                                                                                                
+                                                                                                                                            }
+                                                                                                                                        }
+                                                                                                                                    }
+                                                                                                                                }
+                                                                                                                               }
+                                                                                                                           }
+                                                                                                                       }
+                                                                                                                   }
+                                                                                                                }
+                                                                                                            }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    if(confirm("Зарегестри")) {
+    
+                    }
+                }
+            }
+        }
+    }
+    catch(e) {
+
+    }
+}
